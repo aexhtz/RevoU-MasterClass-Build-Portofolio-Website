cffda6bdea683144102493ae94d418877f1cca62
alfonsodbh <alfonsohutagalung9@gmail.com>
2024-05-24 21:00:37 +0700

    modified:   index.html 	modified:   script.js 	modified:   styles.css

---
+++ script.js
@@ -0,0 +1,19 @@
+const tombolHello = document.querySelector('.buttonsayhello');
+tombolHello.addEventListener('click', () => {
+    window.open('mailto:alfonsohutagalung9@gmail.com', '_blank');
+});
+
+const tombolGithub = document.querySelector('.githubicon');
+tombolGithub.addEventListener('click', () => {
+    window.open('https://github.com/aexhtz', '_blank');
+});
+
+const tombolLinkedin = document.querySelector('.linkedinicon');
+tombolLinkedin.addEventListener('click', () => {
+    window.open('https://linkedin.com/in/alfonsohtg/', '_blank');
+});
+
+const tombolInstagram = document.querySelector('.instagramicon');
+tombolInstagram.addEventListener('click', () => {
+    window.open('https://instagram.com/alfonsohtg', '_blank');
+});
