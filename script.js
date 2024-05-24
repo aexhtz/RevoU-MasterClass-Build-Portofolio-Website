c9ba76c70d958584d0422bf50ed0a5e0682c3087
alfonsodbh <alfonsohutagalung9@gmail.com>
2024-05-24 21:55:23 +0700

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ const tombolInstagram = document.querySelector('.instagramicon');
 tombolInstagram.addEventListener('click', () => {
     window.open('https://instagram.com/alfonsohtg', '_blank');
 });
+
+const tombolAtas1 = document.querySelector('.atas1');
+tombolAtas1.addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+const tombolAtas2 = document.querySelector('.atas2');
+tombolAtas2.addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+const tombolAtas3 = document.querySelector('.atas3');
+tombolAtas3.addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+const tombolBawah1 = document.querySelector('.bawah1');
+tombolBawah1.addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+const tombolBawah2 = document.querySelector('.bawah2');
+tombolBawah2.addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+const tombolBawah3 = document.querySelector('.bawah3');
+tombolBawah3.addEventListener('click', () => {
+    window.open('', '_blank');
+});
